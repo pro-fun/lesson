acc1c9ddfad03b774302f5438874439c1d014c24
pro-fun <novik_vladimir@tut.by>
2023-08-06 22:27:51 +0300

    111

---
+++ src/main/java/by/academypvt/Lesson5.java
@@ -0,0 +1,27 @@
+package by.academypvt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Lesson5 extends HttpServlet {
+    public Lesson5() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.setContentType("text/html");
+        PrintWriter pw =resp.getWriter();
+
+
+    }
+}
